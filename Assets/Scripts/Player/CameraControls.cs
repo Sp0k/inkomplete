d22157fc51746a4da27f2263d5c568e2b9597e79
Sp0k <contact@gabsavard.com>
2026-05-13 01:18:08 -0300

    Added zoom and dezoom to camera

--- Assets/Scripts/Player/CameraControls.cs
+++ Assets/Scripts/Player/CameraControls.cs
@@ -6,32 +6,50 @@ using UnityEngine.InputSystem;
 
 public class CameraControls : MonoBehaviour
 {
+    [Header("Action References")]
     [SerializeField] private InputActionReference _tiltLeftAction;
     [SerializeField] private InputActionReference _tiltRightAction;
+    [SerializeField] private InputActionReference _dezoomAction;
+    [SerializeField] private InputActionReference _zoomAction;
+
+    [Header("Tilt Parameters")]
     [SerializeField] private float _tiltStrength = 3f;
+    [SerializeField] private float _dezoomStrength = 3f;
     private Vector3 offset;
     private Vector3 targetPosition;
 
     public void SetOffset()
     {
-        if (_tiltLeftAction == null || _tiltRightAction == null)
+        if (_tiltLeftAction == null || _tiltRightAction == null || _dezoomAction == null)
         {
             Debug.LogWarning("CameraControls: Input actions not assigned.");
             return;
         }
 
+        float depthOffset = 0f;
+        float sideOffset = 0f;
+
+        if (_dezoomAction.action.IsPressed())
+        {
+            depthOffset -= _dezoomStrength;
+        }
+        
+        if (_zoomAction.action.IsPressed())
+        {
+            depthOffset += _dezoomStrength;
+        }
+
         if (_tiltLeftAction.action.IsPressed())
         {
-            offset = new Vector3(-_tiltStrength, 0f, 0f);
+            sideOffset -= _tiltStrength;
         }
-        else if (_tiltRightAction.action.IsPressed())
+        
+        if (_tiltRightAction.action.IsPressed())
         {
-            offset = new Vector3(_tiltStrength, 0f, 0f);
+            sideOffset += _tiltStrength;
         }
-        else
-        {
-            offset = new Vector3(0f, 0f, 0f);
-        }
+
+        offset = new Vector3(sideOffset, 0f, depthOffset);
     }
 
     private void Update()
